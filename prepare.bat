3fa4038b5fa95d050de351e7424ab383672116c6
swwind <2670529647@qq.com>
2018-04-10 10:20:59 +0800

    update

---
+++ prepare.bat
@@ -0,0 +1,5 @@
+git clone https://github.com/swwind/roigu.git themes/roigu
+copy %cd%\themes\_config.yml %cd%\themes\roigu\_config.yml
+npm install
+copy %cd%\marked.js %cd%\node_modules\marked\lib\marked.js
+pause
